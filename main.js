80d96e2c944921ab02f48d33b3c5065809b29c0a
Marcus <marcus2cu@gmail.com>
2018-04-16 17:15:22 +0100

    Cover for edge case

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ inputVals = (startVal, endVal) => {
     let AP = [];
     let GP = [];
     let d = 2;
+    if (typeof startVal !== 'number' || typeof endVal !== 'number') {
+        console.log('Only numbers are allowed!');
+        return false;
+    }
     for (let i = 0; i < endVal; i++){
     	let AP_formula = startVal + d*i;
         let GP_formula = startVal*d**i;
@@ -15,4 +19,4 @@ inputVals = (startVal, endVal) => {
     }
     console.log(AP, GP);
 }
-inputVals(3, 15);
+inputVals('3', 15);
